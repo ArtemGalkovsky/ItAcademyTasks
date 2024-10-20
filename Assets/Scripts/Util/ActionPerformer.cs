0c998e4093892628fb75f09416e2fa84bc7f168b
ArtemGalkovsky <art_galkovsky@mail.ru>
2024-10-20 17:45:58 +0300

    Renamed argument

--- Assets/Scripts/Util/ActionPerformer.cs
+++ Assets/Scripts/Util/ActionPerformer.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 
 class ActionPerformer
 {
-    public static void PerformActionOnObjects<T>(IEnumerable<T> objectToPerform, Action<T> action)
+    public static void PerformActionOnObjects<T>(IEnumerable<T> objectsToPerform, Action<T> action)
     {
-        foreach (T someObject in objectToPerform)
+        foreach (T someObject in objectsToPerform)
         {
             action(someObject);
         }
