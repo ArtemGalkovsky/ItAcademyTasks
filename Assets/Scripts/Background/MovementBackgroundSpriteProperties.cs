650561090233148f1def5d4196dbe6de9eaf96a6
ArtemGalkovsky <71595194+ArtemGalkovsky@users.noreply.github.com>
2024-11-13 13:18:46 +0300

    Player run sprites were added to the atlas; tile speed now can be -1+

--- Assets/Scripts/Background/MovementBackgroundSpriteProperties.cs
+++ Assets/Scripts/Background/MovementBackgroundSpriteProperties.cs
@@ -4,7 +4,7 @@ namespace Background
 {
     public class MovementBackgroundSpriteProperties : MonoBehaviour
     {
-        [SerializeField, Range(0f, 1f)] private float _speed = 1f;
+        [SerializeField, Range(-1f, 1f)] private float _speed = 1f;
         public float BackgroundSpeed => _speed;
     }
 }
